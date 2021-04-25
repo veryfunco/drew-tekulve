f208701ef3c7c56d4ae33c52e1f061e6850b031f
Andy Mockler <asmockler@gmail.com>
2021-04-25 18:47:06 -0400

    Add filter button bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { InferGetStaticPropsType } from "next";
 
+import { Button } from "../components/Button";
 import { Navbar } from "../components/Navbar";
 import { Page } from "../components/Page";
+import { Stack } from "../components/Stack";
 import { homeData } from "../lib/api";
 
 import styles from "../styles/Home.module.css";
@@ -19,18 +22,40 @@ export const getStaticProps = async () => {
 export default function Home(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  function handleCategoryButtonClick(category: string) {
+    setSelectedCategory(category);
+  }
+
   return (
     <Page title="Work">
       <Navbar />
 
-      <div className={styles.Container}>
+      <div className={styles.HeroContainer}>
         <p>Home</p>
       </div>
 
-      <div>
-        {props.categories.map((category) => (
-          <p key={category.title}>{category.title}</p>
-        ))}
+      <div className={styles.ProjectsContainer}>
+        <Stack>
+          <Button
+            onClick={() => handleCategoryButtonClick(null)}
+            wide
+            transparent={selectedCategory != null}
+          >
+            All
+          </Button>
+
+          {props.categories.map((category) => (
+            <Button
+              transparent={selectedCategory !== category.title}
+              onClick={() => handleCategoryButtonClick(category.title)}
+              key={category.title}
+            >
+              {category.title}
+            </Button>
+          ))}
+        </Stack>
       </div>
     </Page>
   );
