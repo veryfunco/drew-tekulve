5919e68930b7dfd82d4dc24634ceee9d38d1e5d6
Andy Mockler <asmockler@gmail.com>
2021-04-24 23:08:41 -0400

    Site footer (resolves #4)

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -14,12 +14,7 @@ export function Page({ children, title, background = "black" }: Props) {
   const fullTitle = title == null ? "Drew Tekulve" : `Drew Tekulve – ${title}`;
 
   return (
-    <div
-      className={classnames(
-        styles.Page,
-        background === "blue" && styles["Page-blue"]
-      )}
-    >
+    <>
       <Head>
         <title>{fullTitle}</title>
         <link rel="icon" href="/favicon.ico" />
@@ -38,7 +33,59 @@ export function Page({ children, title, background = "black" }: Props) {
         />
       </Head>
 
-      <div className={styles.Container}>{children}</div>
-    </div>
+      <div
+        className={classnames(
+          styles.Page,
+          background === "blue" && styles["Page-blue"]
+        )}
+      >
+        <div className={styles.Container}>
+          {children}
+
+          <footer className={styles.Footer}>
+            <div>
+              Drew Tekulve <br />
+              Freelance Colorist
+            </div>
+
+            <div className={styles.SocialIconsContainer}>
+              <a
+                href="https://instagram.com/drewtekulve"
+                className={styles.SocialIcon}
+                target="_blank"
+              >
+                <img src="/icons/instagram.svg" alt="Instagram" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/drewtekulve"
+                className={styles.SocialIcon}
+                target="_blank"
+              >
+                <img src="/icons/linkedin.svg" alt="LinkedIn" />
+              </a>
+              <a
+                href="https://letterboxd.com/drewtekulve"
+                className={styles.SocialIcon}
+                target="_blank"
+              >
+                <img src="/icons/letterboxd.svg" alt="Letterboxd" />
+              </a>
+              <a
+                href="mailto:drewtekulve@gmail.com"
+                className={styles.SocialIcon}
+                target="_blank"
+              >
+                <img src="/icons/email.svg" alt="Email" />
+              </a>
+            </div>
+
+            <div className={styles.CopyrightContainer}>
+              &copy;{new Date().getFullYear()} All Rights Reserved <br />
+              Tekulve LLC
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
