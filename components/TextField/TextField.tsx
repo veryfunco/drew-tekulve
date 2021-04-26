88ec3f254b7e08485439c8fd8e1eea8f283b4dd2
Andy Mockler <asmockler@gmail.com>
2021-04-26 11:48:31 -0400

    :sparkles: Create TextField

---
+++ components/TextField/TextField.tsx
@@ -0,0 +1,49 @@
+import {
+  useCallback,
+  ChangeEvent,
+  InputHTMLAttributes,
+  TextareaHTMLAttributes,
+} from "react";
+
+import styles from "./TextField.module.css";
+
+interface Props {
+  multiline?: boolean;
+  placeholder: string;
+  required?: boolean;
+  value: string;
+  onChange(value: string): void;
+}
+
+type CommonInputProps = InputHTMLAttributes<HTMLInputElement> &
+  TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+export function TextField({
+  multiline,
+  placeholder,
+  value,
+  onChange,
+  required,
+}: Props) {
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      onChange(event.target.value);
+    },
+    [onChange]
+  );
+
+  const commonProps: CommonInputProps = {
+    placeholder,
+    required,
+    className: styles.Input,
+    onChange: handleChange,
+  };
+
+  return multiline ? (
+    <textarea {...commonProps} rows={5}>
+      {value}
+    </textarea>
+  ) : (
+    <input {...commonProps} value={value} />
+  );
+}
