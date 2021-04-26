88aacf8d96f491014b29c59957ad59c4154963d2
Andy Mockler <asmockler@gmail.com>
2021-04-26 11:47:57 -0400

    Support for vertical stacks

--- components/Stack/Stack.tsx
+++ components/Stack/Stack.tsx
@@ -8,16 +8,24 @@ interface Props {
   align?: "center";
   justify?: "center";
   spacing?: "tight" | "extraTight" | "loose";
+  direction?: "row" | "column";
 }
 
-export function Stack({ children, align, justify, spacing }: Props) {
+export function Stack({
+  children,
+  align,
+  justify,
+  spacing,
+  direction = "row",
+}: Props) {
   return (
     <div
       className={classnames(
         styles.Stack,
         align === "center" && styles["Stack-alignCenter"],
         justify === "center" && styles["Stack-justifyCenter"],
-        spacing && styles[`Stack-${spacing}`]
+        spacing && styles[`Stack-${spacing}`],
+        direction === "column" && styles["Stack-column"]
       )}
     >
       {Children.map(children, (child) => {
