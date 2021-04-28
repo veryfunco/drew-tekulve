02b8df73a726cb586ca8beb93d908118797c4ff5
Andy Mockler <asmockler@gmail.com>
2021-04-27 23:14:35 -0400

    Basic project detail layout

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -8,6 +8,7 @@ import { StaticProps } from "../../types";
 import { projectBySlug } from "../../lib/data/projectBySlug";
 
 import styles from "../../styles/projects/Detail.module.css";
+import { Stack } from "../../components/Stack";
 
 export const getStaticProps = async (
   context: GetStaticPropsContext<{ slug: string }>
@@ -43,8 +44,27 @@ export default function ProjectDetail({
     <Page>
       <Navbar backgroundColor="black" />
 
+      <div className={styles.HeroContainer}>
+        <img className={styles.Thumbnail} src={project.thumbnail} />
+      </div>
+
       <div className={styles.DetailsContainer}>
         <h2 className={styles.Title}>{project.title}</h2>
+
+        <Stack direction="column" spacing="tight">
+          <p>
+            {project.year} – {project.category}
+          </p>
+
+          {project.contributors == null
+            ? null
+            : project.contributors.map(({ name, role }) => (
+                <div key={`${name}-${role}`} className={styles.Contributor}>
+                  <p>{role}</p>
+                  <p style={{ textTransform: "uppercase" }}>{name}</p>
+                </div>
+              ))}
+        </Stack>
       </div>
     </Page>
   );
