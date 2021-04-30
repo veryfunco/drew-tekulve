7243c618fe431bd8a662735b475f29f917f2844e
Andy Mockler <asmockler@gmail.com>
2021-04-30 11:49:10 -0400

    Redirect to admin from next config instead of the hacky useEffect thing

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: true,
+      },
+    ];
+  },
 };
