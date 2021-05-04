049e7b5e5035ef3ae97407c1d90033f54c44943a
Andy Mockler <asmockler@gmail.com>
2021-05-03 22:35:28 -0400

    Add support for youtu.be URLs

--- lib/getVideoEmbedLink.ts
+++ lib/getVideoEmbedLink.ts
@@ -2,17 +2,24 @@ interface Params {
   [key: string]: string;
 }
 
+function youtubeEmbedLink(videoId: string, params: Params) {
+  const urlParams = new URLSearchParams({
+    autoplay: "1",
+    modestbranding: "1",
+    ...params,
+  });
+  return `https://youtube.com/embed/${videoId}?${urlParams}`;
+}
+
 export function getVideoEmbedLink(videoUrl: string, params: Params = {}) {
   const url = new URL(videoUrl);
 
   if (url.host.includes("youtube")) {
     const videoId = url.searchParams.get("v");
-    const urlParams = new URLSearchParams({
-      autoplay: "1",
-      modestbranding: "1",
-      ...params,
-    });
-    return `https://youtube.com/embed/${videoId}?${urlParams}`;
+    return youtubeEmbedLink(videoId, params);
+  } else if (url.host.includes("youtu.be")) {
+    const videoId = url.pathname.split("/")[1];
+    return youtubeEmbedLink(videoId, params);
   } else if (url.host.includes("vimeo")) {
     const videoId = url.pathname.split("/")[1];
     const urlParams = new URLSearchParams({
